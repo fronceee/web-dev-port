8a5ae5039059e8e8ecae382b2b725aed9ff5a665
fronceee <tharathip.tamwiset@gmail.com>
2023-05-04 23:01:03 +0700

    update about data

--- portfolio_2023/src/containers/About.jsx
+++ portfolio_2023/src/containers/About.jsx
@@ -1,46 +1,58 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div id='about' className='flex flex-col mx-6 mb-20 scroll-mt-20'>
-      <h1 className='text-5xl 2xl:pl-16 mb-3 md:mb-8 font-bold text-transparent bg-clip-text bg-gradient-to-br from-orange-500 via-pink-500 to-purple-900'>Who Am I?</h1>
-      <div className='font-light text-sm md:text-base flex flex-col items-center flex justify-center md:flex-row md:gap-14'>
-        <div className='md:w-[49%] flex flex-col gap-4 items-center'>
-          <p className='mb-4 md:m-0'>Hi! My name is Tharathip Tamwiset. 
-            When I use a product, I always consider its process, and web development is the most intriguing to me.
-            My interest in web development began in 2010, when I was in sixth grade and attempted to create a website using Dreamweaver. 
-            I had no idea I was laying the groundwork for web development.
+    <div id="about" className="flex flex-col mx-6 mb-20 scroll-mt-20">
+      <h1 className="text-5xl 2xl:pl-16 mb-3 md:mb-8 font-bold text-transparent bg-clip-text bg-gradient-to-br from-orange-500 via-pink-500 to-purple-900">
+        Who Am I?
+      </h1>
+      <div className="font-light text-sm md:text-base flex flex-col items-center flex justify-center md:flex-row md:gap-14">
+        <div className="md:w-[49%] flex flex-col gap-4 items-center">
+          <p className="mb-4 md:m-0">
+            Hi! My name is Tharathip Tamwiset. When I use a product, I always
+            consider its process, and web development is the most intriguing to
+            me. My interest in web development began in 2010, when I was in
+            sixth grade and attempted to create a website using Dreamweaver. I
+            had no idea I was laying the groundwork for web development.
           </p>
-          <p className='mb-4 md:m-0'>
-            Fast forward to the present. 
-            I completed <a className='link link-hover font-medium' href='https://scrimba.com/certificate/uvP3G5SP/gfrontend' target='_blank'>the Scrimba Front-End Career Path</a>, which provided me with a solid foundation for Front-End Development. 
-            Now I'm at Skooldio, where I'm sharing my programming skills
-            and helping others up-skill and re-skill, 
-            and I'm looking for a job as a Front-End Developer.
+          <p className="mb-4 md:m-0">
+            Fast forward to the present. I completed{" "}
+            <a
+              className="link link-hover font-medium"
+              href="https://scrimba.com/certificate/uvP3G5SP/gfrontend"
+              target="_blank"
+            >
+              the Scrimba Front-End Career Path
+            </a>
+            , which provided me with a solid foundation for Front-End
+            Development. Now I'm at Skooldio, where I'm sharing my programming
+            skills and helping others up-skill and re-skill, and I'm looking for
+            a job as a Front-End Developer.
           </p>
-          <p className='mb-4 md:m-0'>
-            Here are a few technologies and programming languages with which I've recently worked and learned:
+          <p className="mb-4 md:m-0">
+            Here are a few technologies and programming languages with which
+            I've recently worked and learned:
           </p>
-          <div className='flex justify-between mb-8 md:m-0 md:w-1/2 gap-8'>
-            <ul className='list-disc'>
-              <li>JavaScript (ES6+)</li>
+          <div className="flex justify-between mb-8 md:m-0 md:w-1/2 gap-8">
+            <ul className="list-disc">
+              <li>HTML, CSS</li>
               <li>Python</li>
             </ul>
-            <ul className='list-disc'>
-              <li>Node</li>
-              <li>React</li>
+            <ul className="list-disc">
+              <li>JavaScript (ES6+)</li>
+              <li>ReactJs</li>
             </ul>
           </div>
         </div>
-        <div className='group w-full md:w-[31%] flex justify-center'>
-          <div className='relative w-4/6 md:w-content'>
-            <img className='relative z-10' src='./assets/propic2.png' />
+        <div className="group w-full md:w-[31%] flex justify-center">
+          <div className="relative w-4/6 md:w-content">
+            <img className="relative z-10" src="./assets/propic2.png" />
             <div className="absolute -inset-1 bg-gradient-to-br from-orange-500 via-pink-500 to-purple-90 opacity-50 rounded-[50px] blur group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- portfolio_2023/src/data/jobPlaces.js
+++ portfolio_2023/src/data/jobPlaces.js
@@ -1,41 +1,44 @@
 const jobPlaces = [
-    { 
-      place: 'Skooldio',
-      title: `Course Development Associate\n(Tech / Coding)`,
-      time: 'July 2022 - Present',
-      desc: [
-        'Assist the team to deliver quality tech/coding online courses on the platform',
-        'Research and develop tech course markets for current courses and possible offline and online courses',
-        "Answer learners’ questions about content in tech courses via platform's discuss boards and other channels and coordinate more technical questions to instructors.",]
-      },
-    { 
-      place: 'Samsenwittayalai School',
-      title: 'Student Teacher',
-      time: 'Oct 2022 - March 2023',
-      desc: [
-        'Responsible for teaching, lesson planning and creating learning material for M2. English Foundation classes',
-        'Responsible for teaching, lesson planning and creating learning material for M6 Video Editing classes',
-        'Teaching and Deliver both subjects in online and hybrid format.']
-      },
-      { 
-        place: 'Chulalongkorn University Demonstration\nSecondary School',
-        title: 'Student Teacher',
-        time: 'May 2022 - Oct 2022',
-        desc: [
-          'Responsible for teaching, lesson planning and creating learning material for M2. English Foundation classes',
-          'Responsible for teaching, lesson planning and creating learning material for M2 Computing Science classes',
-          'Teaching and Deliver both subjects in online format']
-        },
-      { 
-        place: 'Algorithmics BangkokMetro',
-        title: 'Part-Time Python Teacher',
-        time: 'Dec 2020 - Present',
-        desc: [
-          'Teaching Python for kids aged 12 - 15 who had no pior coding background to be able to create simple coding projects',
-          'Teach Python from basic syntax to complex concepts in programing such as OOP, Dictionary.',
-          'dapt pre-planned lessons to suitable for learners in each class.',
-        ]
-        }
-  ]
+  { 
+    place: 'Skooldio',
+    title: 'Course Development Associate (Tech/Coding)',
+    time: 'July 2022 - Present',
+    desc: [
+      'Assist the team to deliver quality tech/coding online courses on the platform',
+      'Research and developing tech course markets for current courses and possible offline and online courses',
+      "Answer learners' questions about content in tech courses via the platform's discussion boards and other channels and coordinating more technical questions to instructors.",
+    ]
+  },
+  { 
+    place: 'Samsenwittayalai School',
+    title: 'Student Teacher',
+    time: 'Oct 2022 - March 2023',
+    desc: [
+      'Responsible for teaching, lesson planning, and creating learning material for M2 English Foundation classes',
+      'Responsible for teaching, lesson planning, and creating learning material for M6 Video Editing classes',
+      'Teaching and delivering both subjects in online and hybrid formats.'
+    ]
+  },
+  { 
+    place: 'Chulalongkorn University Demonstration Secondary School',
+    title: 'Student Teacher',
+    time: 'May 2022 - Oct 2022',
+    desc: [
+      'Responsible for teaching, lesson planning, and creating learning material for M2 English Foundation classes',
+      'Responsible for teaching, lesson planning, and creating learning material for M2 Computing Science classes',
+      'Teaching and delivering both subjects in online formats.'
+    ]
+  },
+  { 
+    place: 'Algorithmics BangkokMetro',
+    title: 'Part-Time Python Teacher',
+    time: 'Dec 2020 - Present',
+    desc: [
+      'Teaching Python for kids aged 12-15 who have no prior coding background to be able to create simple coding projects',
+      'Teaching Python from basic syntax to complex concepts in programming such as OOP and Dictionary.',
+      'Adapting pre-planned lessons to be suitable for learners in each class.',
+    ]
+  }
+]
 
 export { jobPlaces }
