8c80896d1eb939a872f7cdc3c80f877288717ab7
fronceee <70807925+fronceee@users.noreply.github.com>
2023-07-28 00:06:40 +0700

    Update Footer.jsx
    update inspiration link

--- portfolio_2023/src/containers/Footer.jsx
+++ portfolio_2023/src/containers/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='text-center mb-6 tracking-widest'>
-      <p className='font-extralight text-xs leading-5 link link-hover'><a href='https://brittanychiang.com/' target='_blank'>Design Inspired from Brittany Chiang</a></p>
+      <p className='font-extralight text-xs leading-5 link link-hover'><a href='https://v4.brittanychiang.com/' target='_blank'>Design Inspired from Brittany Chiang</a></p>
       <p className='font-extralight text-xs'>Built by Tharathip Tamwiset</p>
     </div>
   )
