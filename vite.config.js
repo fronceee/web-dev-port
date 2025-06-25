04359a0f07141b90f70cee68bdd09e51ede300d9
Tharathip Tamwiset <70807925+fronceee@users.noreply.github.com>
2025-06-25 15:11:20 +0000

    change build config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/web-dev-port/"
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
+
